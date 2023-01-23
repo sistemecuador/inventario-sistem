dd8186cef40a4e346f09153ae17bc9b0fdace50f
sistemecuador <120998390+sistemecuador@users.noreply.github.com>
2023-01-23 12:23:46 -0500

    Update app.py

--- config/app.py
+++ config/app.py
@@ -11,7 +11,7 @@ from config.settings import config
 
 def create_app():
     app = Flask(__name__, static_folder=BASE_STATIC_ROOT, template_folder=BASE_TEMPLATE_ROOT)
-    app.config.from_object(config['developer'])
+    app.config.from_object(config['productions'])
     db.init_app(app)
     app.register_blueprint(users)
     app.register_blueprint(login)
